98e033c402bdf37b039a1bf95d60b10d1a036de9
Martin Imobersteg <martin.imobersteg@gmail.com>
2023-04-11 17:03:26 +0200

    initial commit

---
+++ src/main/java/com/example/beatlessonggenerator/BeatlesSongGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.beatlessonggenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatlesSongGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatlesSongGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beatlessonggenerator/BeatlesSongGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beatlessonggenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatlesSongGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
